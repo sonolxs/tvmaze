ae0833857584d077eb69c26b1da3785993893624
sonolxs <alexis.castilloal99@gmail.com>
2026-09-11 14:38:13 -0600

    start

---
+++ src/main/java/com/example/tvmaze/TvmazeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tvmaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvmazeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tvmaze/TvmazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tvmaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
